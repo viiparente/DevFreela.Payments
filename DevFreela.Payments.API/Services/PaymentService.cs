65f7cc4e75fabcb4d7a634fea6955fbf9aa52e8a
Vinicius Parente <vd.parente@hotmail.com>
2022-01-19 20:49:13 -0300

    Created Project

---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,13 @@
+﻿using DevFreela.Payments.API.Models;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            //Somente para demonstração
+            return Task.FromResult(true);
+        }
+    }
+}
